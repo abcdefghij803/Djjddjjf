74f88b55be42741f1788d395fe2a39ba9dc7edbc
abcdefghij803 <analtamash1@gmail.com>
2025-09-03 18:08:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 API_ID = 20210979       # your api id
 API_HASH = "8cc33da16acda11d393c0cfffab8c1a0"
 MONGO_URL = "mongodb+srv://rahul:rahulkr@cluster0.szdpcp6.mongodb.net/?retryWrites=true&w=majority"
-BOT_TOKEN = "your_bot_token_here"   # BotFather se
+BOT_TOKEN = "8204482008:AAH_QFPc5OddsmmyhsP0KcMESftRRwBAQeo"   # BotFather se
